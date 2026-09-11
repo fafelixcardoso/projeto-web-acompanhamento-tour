16dc63b426ce1d71eb663a97b6364d840f4fab5c
fafelixcardoso <fabriciofcjunior@gmail.com>
2026-09-11 17:20:52 -0300

    feat(repo): versão inicial

---
+++ src/main/java/br/uel/acomp_tour/AcompTourApplication.java
@@ -0,0 +1,13 @@
+package br.uel.acomp_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uel/acomp_tour/AcompTourApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uel.acomp_tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
