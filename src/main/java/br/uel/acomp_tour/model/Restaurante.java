f856a0e96798a4dbed91c33b96feca8b2825f49b
Guilherme T. da Silva <guitanigawa07@gmail.com>
2026-09-14 23:11:09 -0300

    feat(repo): estruturação inicial da camada Model

--- src/main/java/br/uel/acomp_tour/model/Restaurante.java
+++ src/main/java/br/uel/acomp_tour/model/Restaurante.java
@@ -1,5 +1,56 @@
 package br.uel.acomp_tour.model;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+
+// Não vamos conseguir testar a validação e o cadastro no banco pq
+// não tem controller ainda, mas acho melhor ir criando assim direto doq fazer
+// td e ir mudando dps
+
+@Entity
+@Table(name = "restaurantes")
+@JsonPropertyOrder({
+        "id", "nome", "endereco", "comentario", "economia", "avaliacao", "dataVisita"
+})
 public class Restaurante {
+
+    // Atributos dos restaurantes (provisório)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message="O nome do restaurante não deve ser nulo/vazio")
+    @Size(max = 150, message = "Nome do restaurante muito longo!")
+    private String nome;
+
+
+    // Deu preg, amnh continuo😋
+    private String endereco;
+    private String comentario;
+    private Float economia;
+    private Float avaliacao;
+    private LocalDate dataVisita;
+
+
+    // Getters e setters
+    public Long getId(){ return id; }
+    public String getNome(){ return nome; }
+    public String getEndereco(){ return endereco; }
+    public String getComentario(){ return comentario; }
+    public Float getEconomia(){ return economia; }
+    public Float getAvaliacao(){ return avaliacao; }
+    public LocalDate getDataVisita(){ return dataVisita; }
+
+    public void setNome(String nome){ this.nome = nome; }
+    public void setEndereco(String endereco){ this.endereco = endereco; }
+    public void setComentario(String comentario){ this.comentario = comentario; }
+    public void setEconomia(Float economia){ this.economia = economia; }
+    public void setAvaliacao(Float avaliacao){ this.avaliacao = avaliacao; }
+    public void setDataVisita(LocalDate dataVisita){ this.dataVisita = dataVisita; }
 
 }
--- src/main/java/br/uel/acomp_tour/repository/RestauranteRepository.java
+++ src/main/java/br/uel/acomp_tour/repository/RestauranteRepository.java
@@ -1,4 +1,9 @@
 package br.uel.acomp_tour.repository;
 
-public class RestauranteRepository {
+import br.uel.acomp_tour.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestauranteRepository
+    extends JpaRepository<Restaurante, Long> {
+        // Nada aqui por enquanto, mas acho q vamos precisar mexer dps
 }
