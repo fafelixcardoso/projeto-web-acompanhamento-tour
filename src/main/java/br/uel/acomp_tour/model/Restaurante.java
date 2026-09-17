a9b273dcbb02dffd7d57883cd7a19903c2b5e77e
fafelixcardoso <fabriciofcjunior@gmail.com>
2026-09-17 09:52:05 -0300

    feat(repo): Model praticamente finalizada

--- src/main/java/br/uel/acomp_tour/model/Restaurante.java
+++ src/main/java/br/uel/acomp_tour/model/Restaurante.java
@@ -28,12 +28,23 @@ public class Restaurante {
     @Size(max = 150, message = "Nome do restaurante muito longo!")
     private String nome;
 
-
-    // Deu preg, amnh continuo😋
+    @NotBlank(message="O endereço do restaurante é obrigatório")
+    @Size(max = 200, message = "Endereço do restaurante muito longo!")
     private String endereco;
+
+    @Size(max = 500, message = "Comentário muito longo!")
     private String comentario;
+
+    @NotNull(message = "O valor de economia é obrigatório")
+    @Positive(message = "O valor deve ser positivo")
     private Float economia;
+
+    @NotNull(message = "A avaliação é obrigatória")
+    @Min(value = 0, message = "A avaliação mínima é 0")
+    @Max(value = 5, message = "A avaliação máxima é 5")
     private Float avaliacao;
+
+    @PastOrPresent(message = "A data da visita não pode estar no futuro")
     private LocalDate dataVisita;
 
 
@@ -46,11 +57,11 @@ public class Restaurante {
     public Float getAvaliacao(){ return avaliacao; }
     public LocalDate getDataVisita(){ return dataVisita; }
 
+    public void setId(Long id){ this.id = id; }
     public void setNome(String nome){ this.nome = nome; }
     public void setEndereco(String endereco){ this.endereco = endereco; }
     public void setComentario(String comentario){ this.comentario = comentario; }
     public void setEconomia(Float economia){ this.economia = economia; }
     public void setAvaliacao(Float avaliacao){ this.avaliacao = avaliacao; }
     public void setDataVisita(LocalDate dataVisita){ this.dataVisita = dataVisita; }
-
 }
