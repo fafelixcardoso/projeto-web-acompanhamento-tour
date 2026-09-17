b6a6ecb0475d7ca0dbb302d0839f0ce63a10e4e3
fafelixcardoso <fabriciofcjunior@gmail.com>
2026-09-17 20:42:40 -0300

    feat(repo): @Repository adicionado

--- src/main/java/br/uel/acomp_tour/repository/RestauranteRepository.java
+++ src/main/java/br/uel/acomp_tour/repository/RestauranteRepository.java
@@ -2,10 +2,12 @@ package br.uel.acomp_tour.repository;
 
 import br.uel.acomp_tour.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface RestauranteRepository
     extends JpaRepository<Restaurante, Long> {
 
