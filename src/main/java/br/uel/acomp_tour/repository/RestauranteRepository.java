fa880ddd77e6c31e29782115694abf39d5b20ddd
Guilherme T. da Silva <guitanigawa07@gmail.com>
2026-09-17 18:17:06 -0300

    feat(repo): início da service

--- src/main/java/br/uel/acomp_tour/repository/RestauranteRepository.java
+++ src/main/java/br/uel/acomp_tour/repository/RestauranteRepository.java
@@ -3,7 +3,25 @@ package br.uel.acomp_tour.repository;
 import br.uel.acomp_tour.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface RestauranteRepository
     extends JpaRepository<Restaurante, Long> {
-        // Nada aqui por enquanto, mas acho q vamos precisar mexer dps
+
+    // Filtrar por nome (id é nativo)
+    List<Restaurante> findByNomeContainingIgnoreCase(String nome);
+
+    // Filtrar por valor fixo
+    List<Restaurante> findByEconomia(Float economia_min);
+    List<Restaurante> findByAvaliacao(Float avaliacao);
+    List<Restaurante> findByDataVisita(LocalDate data);
+
+    // Filtrar por range
+    List<Restaurante> findByEconomiaBetween(Float economia_min, Float economia_max);
+    List<Restaurante> findByAvaliacaoBetween(Float avaliacao_min, Float avaliacao_max);
+    List<Restaurante> findByDataVisitaBetween(LocalDate data_inicial, LocalDate data_final);
+
+
+
 }
--- src/main/java/br/uel/acomp_tour/service/RestauranteService.java
+++ src/main/java/br/uel/acomp_tour/service/RestauranteService.java
@@ -1,4 +1,43 @@
 package br.uel.acomp_tour.service;
 
+import br.uel.acomp_tour.model.Restaurante;
+import br.uel.acomp_tour.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
 public class RestauranteService {
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    public List<Restaurante> listarRestaurantes(){
+        return restauranteRepository.findAll();
+    }
+
+    // Se tu achar q ficou mt feio pd mudar
+    public List<Restaurante> listarRestaurantesOrdenado(String ordem, String atributo){
+        Sort sort_atr =  Sort.by(atributo);
+        Sort sort_atr_ordem = ordem.equals("desc") ? sort_atr.descending() : sort_atr.ascending();
+
+        return restauranteRepository.findAll(sort_atr_ordem);
+    }
+
+    public Restaurante buscarRestauranteId(Long id){
+        return restauranteRepository.findById(id).orElse(null);
+    }
+
+    public List<Restaurante> buscarRestaurantePorNome(String nome){
+        return restauranteRepository.findByNomeContainingIgnoreCase(nome);
+    }
+
+
+
+
+
+
+
 }
